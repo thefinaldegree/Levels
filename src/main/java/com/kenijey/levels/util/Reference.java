d5ef60f0db12dfb55c49ff9e55f1162c3b9e127f
Keny <mylankca1995@gmail.com>
2018-02-24 13:29:31 +0300

    version 2.0.1 to be released

--- src/main/java/com/kenijey/levels/util/Reference.java
+++ src/main/java/com/kenijey/levels/util/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 {
 	public static final String MODID = "levels";
 	public static final String NAME = "Levels";
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.0.1";
 	public static final String COMMON = "com.kenijey.levels.proxies.CommonProxy";
 	public static final String CLIENT = "com.kenijey.levels.proxies.ClientProxy";
 }
