2d04b30d210a666060b07991e6bd45def53bd21e
Keny <mylankca1995@gmail.com>
2018-06-07 00:04:47 +0300

    version 2.4.0 to be released

--- src/main/java/com/kenijey/levels/util/Reference.java
+++ src/main/java/com/kenijey/levels/util/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 {
 	public static final String MODID = "weaponlevels";
 	public static final String NAME = "Weapon Levels";
-	public static final String VERSION = "2.3.5";
+	public static final String VERSION = "2.4.0";
 	public static final String COMMON = "com.kenijey.levels.proxies.CommonProxy";
 	public static final String CLIENT = "com.kenijey.levels.proxies.ClientProxy";
 }
